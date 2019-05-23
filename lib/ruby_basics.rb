ecaf2df760ac56809fbe1da668f8ea7c27e383d7
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-23 23:54:58 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,7 +10,7 @@ def argue (phrase="I'm right and you are wrong!")
   return phrase
 end
 
-def greeting ( greeting, name = "Bobby!")
+def greeting (name, greeting = "Hi there, ")
   return greeting + name
 end
 
